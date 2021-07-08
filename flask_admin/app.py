cef5231b68b8957c1ad7245343fd17f43b16d4a5
Petros <Xavrais312@gmail.com>
2021-07-08 22:20:52 +0300

    Add BASIC security

--- flask_admin/app.py
+++ flask_admin/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from flask_admin import Admin
 from flask_admin import BaseView, expose
@@ -8,10 +9,28 @@ from pymongo_insert import get_database
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin.contrib.sqla import ModelView
 
+from flask_security import Security, login_required, \
+     SQLAlchemySessionUserDatastore
+from database import db_session, init_db
+from models import User, Role
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/linp/A_LEARN/flask_admin/ftest.sqlite'
-app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['SECURITY_PASSWORD_SALT'] = os.environ.get("SECURITY_PASSWORD_SALT", '146585145368132386173505678016728509634')
+
+user_datastore = SQLAlchemySessionUserDatastore(db_session,
+                                                User, Role)
+security = Security(app, user_datastore)
+
+#@app.before_first_request
+def create_user():
+    init_db()
+    user_datastore.create_user(email='pursu@test.net', password='password')
+    db_session.commit()
+
 print(app.config['SQLALCHEMY_DATABASE_URI'])
 db = SQLAlchemy(app)
 # set optional bootswatch theme
@@ -24,16 +43,18 @@ class Items(db.Model):
     name = db.Column(db.String(30))
 # Add administrative views here
 
-@app.route("/", methods=["GET"])
+@app.route("/")
+@login_required
 def index():
     return render_template("index.html")
 
+'''
 class NotificationsView(BaseView):
     @expose('/')
     def index(self):
         return self.render('admin/notify.html')
-
 admin.add_view(NotificationsView(name='Notifications', endpoint = 'notify'))
+'''
 admin.add_view(ModelView(Items, db.session))
 
-app.run(debug = True)
+app.run()
